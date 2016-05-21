8cc0a52a74a570dbfdf0e2a4d259e6de5bde61f1
Byron Mejia <byron.mejia@connect.qut.edu.au>
2016-05-21 19:50:13 +1000

    He wasn't ready

--- back/server.rb
+++ back/server.rb
@@ -1,10 +1,11 @@
-require 'JSON'
+require 'json'
 
 require_relative 'actors/client_collection'
-require_relative 'actors/socket_client'
+# require_relative 'actors/socket_client'
 require_relative 'actors/web_server'
 require_relative 'actors/arduino_controller'
 
+puts 'Parsing Config...'
 config_file = File.read('config/config.json')
 config = JSON.parse(config_file)
 
