a360b822c9f7b58b36a6b6aa15bfc9a55c23ba74
Byron Mejia <byron.mejia@connect.qut.edu.au>
2016-07-06 19:14:26 +1000

    goodbye

--- front/vanilla.js
+++ front/vanilla.js
@@ -42,15 +42,6 @@ function MessageFactory() {
     }
 }
 
-function getRandomColor() {
-    var letters = '0123456789ABCDEF'.split('');
-    var color = '#';
-    for (var i = 0; i < 6; i++ ) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
-
 // -------------------- Variables --------------------
 
 var exampleSocket = new WebSocket("ws://localhost:9292");
